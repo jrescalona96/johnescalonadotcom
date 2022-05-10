e23a7da51231509cd6c86d37a96f1a51460e4398
John Escalona <johnrichmondescalona@gmail.com>
2022-05-09 22:35:06 -0700

    Revise navbar styling

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -2,12 +2,11 @@ import { TextLink } from "../Link";
 import "./navBar.css";
 
 export const NavBar = () => {
-	const classes: string = "hover:motion-safe:animate-pulse";
+	const classes: string = "hover:motion-safe:animate-pulse decoration-2";
 	const isActive = (path: string): string => {
-		return window.location.pathname === path
-			? " underline decoration-2 decoration-sky-500/30"
-			: "";
+		return window.location.pathname === path ? " underline" : "";
 	};
+
 	return (
 		<nav className="flex flex-row gap-x-4 py-6">
 			<TextLink className={classes} href="/">
