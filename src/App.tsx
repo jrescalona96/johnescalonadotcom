52b3dda7ca11cdb94cd62dda08fede8eb6492450
John Escalona <johnrichmondescalona@gmail.com>
2022-05-09 23:40:05 -0700

    Added navigation for subpages

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,36 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "./screens/home/Home";
-import { Contact } from "./screens/contact/Contact";
-import { Interests } from "./screens/interests/Interests";
+import { StringConstants as Strings } from "./assets/constants/StringConstants";
+import { Home } from "./pages/Home";
+import { Contact } from "./pages/Contact";
+import { Interests } from "./pages/Interests";
+import { Projects } from "./pages/Projects";
+import { Coffee } from "./pages/Coffee";
+import { Fitness } from "./pages/Fitness";
+import { Nature } from "./pages/Nature";
 
 import "./App.css";
 
 function App() {
 	return (
-		<div className="App">
+		<div className="App container h-screen my-auto mx-auto max-w-4xl">
 			<BrowserRouter>
 				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/contact" element={<Contact />} />
-					<Route path="/interests" element={<Interests />} />
+					<Route path={Strings.HOME_PATH} element={<Home />} />
+					<Route path={Strings.CONTACT_PATH} element={<Contact />} />
+					<Route path={Strings.INTERESTS_PATH} element={<Interests />} />
+					<Route
+						path={Strings.INTERESTS_PATH + Strings.COFFEE_PATH}
+						element={<Coffee />}
+					/>
+					<Route
+						path={Strings.INTERESTS_PATH + Strings.FITNESS_PATH}
+						element={<Fitness />}
+					/>
+					<Route
+						path={Strings.INTERESTS_PATH + Strings.NATURE_PATH}
+						element={<Nature />}
+					/>
+					<Route path={Strings.PROJECTS_PATH} element={<Projects />} />
 				</Routes>
 			</BrowserRouter>
 		</div>
--- src/screens/contact/Contact.tsx
+++
@@ -1,7 +0,0 @@
-export const Contact = () => {
-	return (
-		<section>
-			<h1>Contact</h1>
-		</section>
-	);
-};
