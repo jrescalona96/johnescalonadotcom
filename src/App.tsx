fb2f1f7a7437732c6fd75a8020476f98daddfc8b
John Escalona <johnrichmondescalona@gmail.com>
2022-05-10 00:07:28 -0700

    endpoint lookup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { StringConstants as Strings } from "./assets/constants/StringConstants";
+import { Endpoints } from "./assets/constants/endpoints";
 import { Home } from "./pages/Home";
 import { Contact } from "./pages/Contact";
 import { Interests } from "./pages/Interests";
 import { Projects } from "./pages/Projects";
 import { Coffee } from "./pages/Coffee";
 import { Fitness } from "./pages/Fitness";
-import { Nature } from "./pages/Nature";
+import { Camping } from "./pages/Camping";
 
 import "./App.css";
 
@@ -15,22 +15,13 @@ function App() {
 		<div className="App container h-screen my-auto mx-auto max-w-4xl">
 			<BrowserRouter>
 				<Routes>
-					<Route path={Strings.HOME_PATH} element={<Home />} />
-					<Route path={Strings.CONTACT_PATH} element={<Contact />} />
-					<Route path={Strings.INTERESTS_PATH} element={<Interests />} />
-					<Route
-						path={Strings.INTERESTS_PATH + Strings.COFFEE_PATH}
-						element={<Coffee />}
-					/>
-					<Route
-						path={Strings.INTERESTS_PATH + Strings.FITNESS_PATH}
-						element={<Fitness />}
-					/>
-					<Route
-						path={Strings.INTERESTS_PATH + Strings.NATURE_PATH}
-						element={<Nature />}
-					/>
-					<Route path={Strings.PROJECTS_PATH} element={<Projects />} />
+					<Route path={Endpoints.home} element={<Home />} />
+					<Route path={Endpoints.projects} element={<Projects />} />
+					<Route path={Endpoints.contact} element={<Contact />} />
+					<Route path={Endpoints.interests} element={<Interests />} />
+					<Route path={Endpoints.coffee} element={<Coffee />} />
+					<Route path={Endpoints.fitness} element={<Fitness />} />
+					<Route path={Endpoints.camping} element={<Camping />} />
 				</Routes>
 			</BrowserRouter>
 		</div>
