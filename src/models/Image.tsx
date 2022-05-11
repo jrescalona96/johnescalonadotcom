9fac3cff67cd9b31178832d063eb4892234a53af
John Escalona <johnrichmondescalona@gmail.com>
2022-05-10 22:44:19 -0700

    Refactor Header and Navbar

--- src/models/Image.tsx
+++ src/models/Image.tsx
@@ -1,5 +1,16 @@
+type ImageParameters = {
+	id: number;
+	src: string;
+	label: string;
+};
+
 export class Image {
-	src!: string;
-	label?: string;
-	emoji?: string;
+	public id!: number;
+	public src!: string;
+	public label?: string;
+	constructor({ id, src, label }: ImageParameters) {
+		this.id = id;
+		this.src = src;
+		this.label = label;
+	}
 }
