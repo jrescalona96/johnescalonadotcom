c497a7364fc58a9fbf3bbfd86971da521006f525
John Escalona <johnrichmondescalona@gmail.com>
2022-10-18 22:46:12 -0700

    Added Splash Page

---
+++ src/pages/SplashPage.tsx
@@ -0,0 +1,34 @@
+import Lottie from "lottie-react";
+// import coffeeTea from "../assets/lottie/coffeetea.json";
+// import pourOverCoffee from "../assets/lottie/pourover-coffee.json";
+// import aeroPressCoffee from "../assets/lottie/aeropress-coffee.json";
+import hiAnimation from "../assets/lottie/hi-loader.json";
+import { useState } from "react";
+
+export const SplashPage = ({ isVisible }: { isVisible: boolean }) => {
+	let baseClasses =
+		"transition-all ease-in-out h-screen w-screen flex bg-white justify-center z-10";
+	const [classes, setClasses] = useState(baseClasses);
+
+	if (isVisible) {
+		return (
+			<div className={classes}>
+				<Lottie
+					className="invisible md:visible flex my-auto"
+					animationData={hiAnimation}
+					loop={false}
+					autoplay={true}
+					onComplete={() =>
+						setClasses(baseClasses + " opacity-0 duration-1000")
+					}
+					rendererSettings={{ preserveAspectRatio: "xMidYMid slice" }}
+					style={{
+						overflow: "hidden",
+						width: "30%",
+					}}
+				/>
+			</div>
+		);
+	}
+	return <></>;
+};
